ec1be0263ef5fb8b5a4f4f56bd1419cfe0cd529b
AyushKazi <ayushkazishrestha@gmail.com>
2024-02-23 15:33:31 +0545

    sub total and total via reducers

--- frontend/slices/invoiceSlice.js
+++ frontend/slices/invoiceSlice.js
@@ -92,6 +92,29 @@ const initialState = {
   subTotal: "",
 };
 
+const calculateSubTotal = (products) => {
+  return products.reduce((total, product) => {
+    const amount = product.amount !== "" ? parseInt(product.amount) : 0;
+    return total + amount;
+  }, 0);
+};
+
+const calculateTotal = (discount, subTotal, tax) => {
+  let currentDiscount = parseInt(discount);
+  let currentAmount = subTotal;
+  let currentTax = parseInt(tax);
+  if (currentAmount > 0) {
+    let total = currentAmount;
+
+    currentDiscount > 0 && (total -= (currentDiscount / 100) * currentAmount);
+    currentTax > 0 && (total += (tax / 100) * total);
+
+    console.log(total);
+    return total;
+  }
+  return "";
+};
+
 const invoiceSlice = createSlice({
   name: "invoice",
   initialState,
@@ -99,28 +122,41 @@ const invoiceSlice = createSlice({
     changeFormData: (state, action) => {
       state[action.payload.name] = action.payload.value;
     },
+    // to update the service/products row
     updateProducts: (state, action) => {
       //code to update products
       //  state.products[index] -> this refers to the product of the given index
       const { name, value, index } = action.payload;
-      state.products[index][name] = value; //
 
-      // Recalculate amount if numberOfItems or rate changes
-      if (name === "numberOfItems" || name === "rate") {
-        const numberOfItems =
-          parseInt(state.products[index].numberOfItems) || 1;
-        const rate = parseFloat(state.products[index].rate) || 0;
-        state.products[index].amount = (numberOfItems * rate).toFixed(2); // Assuming you want to keep 2 decimal places
+      if (index > -1) {
+        state.products[index][name] = value; //
+
+        // Recalculate amount if numberOfItems or rate changes
+        if (name === "numberOfItems" || name === "rate") {
+          const numberOfItems =
+            parseInt(state.products[index].numberOfItems) || 1;
+          const rate = parseFloat(state.products[index].rate) || 0;
+          state.products[index].amount = (numberOfItems * rate).toFixed(2); // Assuming you want to keep 2 decimal places
+        }
+      } else {
+        state[name] = value;
       }
+      state.subTotal = calculateSubTotal(state.products);
+      const { discount, tax } = state;
+      state.total = calculateTotal(discount, state.subTotal, tax);
     },
+    // to add a row of the service/product
     addItem: (state, action) => {
       state.products = [...state.products, defaultProduct];
+      state.subTotal = calculateSubTotal(state.products);
     },
+    // to delete the row
     removeItem: (state, action) => {
       const indexToDelete = action.payload;
       state.products = state.products.filter(
         (_, index) => index !== indexToDelete
       );
+      state.subTotal = calculateSubTotal(state.products);
     },
   },
 });
@@ -141,3 +177,8 @@ export default invoiceSlice.reducer;
 // likwise kun page ma data access garnu xa, tesma useSelector use garni
 // useSelector (state) => {}, yesko state le global state access garxa ani state.invoice or state.posts
 // kun slice ko data access garnu xa yesari access garni (state.)
+
+//in configure store ,, we store reducers, which has initial value and actions
+// and in reducers, we have name, initial value and reducers
+// useSelector (state.{name of reducer}) to retreve the values of that reducer state values
+// useDispatch to dispatch the action ( dispatch(addItem()))
